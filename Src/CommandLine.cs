ed734fd932a79adfbb05d6b984f44cd986922a8a
rstarkov <sourcecode@roman.st>
2013-11-26 11:29:14 +0000

    Initial version.

---
+++ Src/CommandLine.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using RT.Util;
+using RT.Util.CommandLine;
+using RT.Util.Consoles;
+using RT.Util.ExtensionMethods;
+
+namespace OldFiles
+{
+    sealed class CommandLine : ICommandLineValidatable
+    {
+#pragma warning disable 0649 // Field is never assigned to
+
+        [DocumentationRhoML("{h}Restrict analysis to files whose names match this regular expression.{}")]
+        [Option("-f", "--filter")]
+        public string Filter;
+
+        [DocumentationRhoML("{h}Recursively process subdirectories.{}\nWhen specified, all files in all directories are grouped by name and considered together. When omitted, each folder passed to {field}Folders{} is considered separately from other folders.")]
+        [Option("-r", "--recursive")]
+        public bool Recursive;
+
+        [DocumentationRhoML("{h}All files older than {field}Age{} should be deemed old.{}\nSpecified in days; fractions are permitted.")]
+        [Option("-m", "--max-age")]
+        public double MaxAge = double.MaxValue;
+
+#warning Expand documentation here
+        [DocumentationRhoML("{h}All files spaced more closely than specified in {field}Spacing{} should be deemed old.{}")]
+        [Option("-s", "--spacing")]
+        public string Spacing;
+
+        [DocumentationRhoML("{h}A regular expression specifying how timestamps should be parsed.{}\nThe regex must contain named groups {h}y{}, {h}m{}, {h}d{}, and may also contain groups {h}th{}, {h}tm{}, {h}ts{}. Only files whose names match this regex are analysed. Matched parts are highlighted white in the verbose output. The default format matches timestamps like {h}\"YYYY-MM-DD\"{}, {h}\"YYYY-MM-DD.hh-mm-ss\"{} and {h}\"YYYY-MM-DD hhmmss\"{} (seconds optional).\nIf a group named {h}g{} is present, the matched string is used to group the files. Multiple groups may be named {h}g{} in the same regex if necessary. The matched part is highlighted yellow in the verbose output.")]
+        [Option("-t", "--timestamp")]
+        public string TimestampFormat;
+
+        [DocumentationRhoML("{h}Print extra information.{}\nIf specified, OldFiles will list the names of files that aren't deemed old, file group names, and will display the output of the {option}--execute{} command, if any, for each old file.")]
+        [Option("-v", "--verbose")]
+        public bool Verbose;
+
+        [DocumentationRhoML("{h}Delete all files that are deemed old.{}")]
+        [Option("-d", "--delete")]
+        public bool Delete;
+
+        [DocumentationRhoML("{h}Execute the specified OS command for all files that are deemed old.{}\nThe command is executed once for each file, and is formed by replacing the string \"{h}{{}{}\" with the file path. Where {option}--delete{} is also specified, the file will be deleted only if this command exits with the status code 0.")]
+        [Option("-e", "--execute")]
+        public string Execute;
+
+        [DocumentationRhoML("{h}The folder(s) to be scanned for old files.{}")]
+        [IsPositional, IsMandatory]
+        public string[] Folders;
+
+        [Ignore]
+        public Regex FilterRegex;
+
+        [Ignore]
+        public Func<double, double> SpacingFunc;
+
+        [Ignore]
+        public Regex TimestampFormatRegex;
+
+#pragma warning restore 0649 // Field is never assigned to
+
+        public ConsoleColoredString Validate()
+        {
+            var result = validate();
+            if (result == null)
+                return null;
+            else
+                return CommandLineParser.Colorize(RhoML.Parse(result));
+        }
+
+        private string validate()
+        {
+            if (Filter != null)
+            {
+                try { FilterRegex = new Regex(Filter, RegexOptions.IgnoreCase | RegexOptions.Singleline); }
+                catch (Exception e) { return "The value you provided for {field}Filter{} is not a valid regular expression: {0}".Fmt(RhoML.Escape(e.Message)); }
+            }
+
+            if (TimestampFormat == null)
+                TimestampFormatRegex = new Regex(@"(?<y>\d\d\d\d)-(?<m>\d\d)-(?<d>\d\d)   ( (\.+(?<th>\d\d)-(?<tm>\d\d)(-(?<ts>\d\d))?)  |  (\s+(?<th>\d\d)(?<tm>\d\d)(?<ts>\d\d)?) )?", RegexOptions.Singleline | RegexOptions.IgnorePatternWhitespace);
+            else
+            {
+                try { TimestampFormatRegex = new Regex(TimestampFormat, RegexOptions.IgnoreCase | RegexOptions.Singleline); }
+                catch (Exception e) { return "The value you provided for {field}TimestampFormat{} is not a valid regular expression: {0}".Fmt(RhoML.Escape(e.Message)); }
+                var names = TimestampFormatRegex.GetGroupNames();
+                if (!names.Contains("y") || !names.Contains("m") || !names.Contains("d"))
+                    return @"The regular expression for {field}TimestampFormat{} must include the named capture groups ""y"", ""m"" and ""d"" for year, month and day.";
+                if ((names.Contains("tm") && !names.Contains("th")) || (names.Contains("ts") && !names.Contains("tm")))
+                    return @"The regular expression for {field}TimestampFormat{} may only include the named capture group ""tm"" only if ""th"" is also present. Similarly, the group ""ts"" requires ""tm"".";
+            }
+
+            if (Spacing == null)
+                SpacingFunc = age => 0;
+            else
+            {
+                if (Spacing.StartsWith("fixed:", StringComparison.OrdinalIgnoreCase))
+                {
+                    // --spacing fixed:15
+                    var value = Spacing.Substring(6);
+                    double spacing;
+                    if (!double.TryParse(value, out spacing))
+                        return @"Cannot parse the {field}Spacing{} parameter: the {h}fixed{} value should be a number.";
+                    SpacingFunc = age => spacing;
+                }
+                else if (Spacing.StartsWith("list:", StringComparison.OrdinalIgnoreCase))
+                {
+                    // --spacing list:[5,0.15age][15,2]
+                    var value = Spacing.Substring(5);
+                    var matches = Regex.Matches(value, @"\[(?<limit>[\d.]+)\,(?<value>[\d.]+)(?<rel>age)?\]", RegexOptions.Singleline | RegexOptions.ExplicitCapture).Cast<Match>();
+                    // Make sure there are no extraneous characters
+                    int cur = 0;
+                    foreach (var match in matches)
+                    {
+                        if (match.Index != cur)
+                            return @"Cannot parse the {field}Spacing{} parameter. Extraneous characters in the {h}list{} specifier: ""{0}""".Fmt(RhoML.Escape(value.Substring(cur, match.Index - cur)));
+                        cur = match.Index + match.Length;
+                    }
+                    if (cur != value.Length)
+                        return @"Cannot parse the {field}Spacing{} parameter. Extraneous characters in the {h}list{} specifier: ""{0}""".Fmt(RhoML.Escape(value.Substring(cur)));
+                    // Construct the spacing function
+                    var list = matches
+                        .Select(m => new { Limit = double.Parse(m.Groups["limit"].Value), Val = double.Parse(m.Groups["value"].Value), Relative = m.Groups["rel"].Success })
+                        .OrderBy(m => m.Limit)
+                        .ToArray();
+                    SpacingFunc = age =>
+                    {
+                        var relevant = list.FirstOrDefault(v => v.Limit <= age);
+                        if (relevant == null)
+                            return 0; // if there is no entry with a limit of 0, all files younger than the first limit are never considered old.
+                        return relevant.Relative ? (relevant.Val * age) : (relevant.Val);
+                    };
+                }
+                else
+                    return @"Cannot parse the {field}Spacing{} parameter.";
+            }
+
+            foreach (var folder in Folders)
+            {
+                if (!Directory.Exists(folder))
+                    return @"Folder not found: {h}{0}{}".Fmt(RhoML.Escape(folder));
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Src/Program.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using RT.Util;
+using RT.Util.CommandLine;
+using RT.Util.Consoles;
+using RT.Util.ExtensionMethods;
+
+namespace OldFiles
+{
+    class Program
+    {
+        private static CommandLine Args;
+        private static bool HadProblems = false;
+        private static DateTime Now;
+
+        private static int Main(string[] args)
+        {
+            if (args.Length == 2 && args[0] == "--post-build-check")
+                return Ut.RunPostBuildChecks(args[1], Assembly.GetExecutingAssembly());
+
+            Args = CommandLineParser.ParseOrWriteUsageToConsole<CommandLine>(args);
+            if (Args == null)
+                return 1;
+
+            Now = DateTime.Now; // so that it's fixed throughout the entire run
+            if (Args.Recursive)
+                processFolder(findAllFilesRecursive());
+            else
+                foreach (var folder in Args.Folders)
+                    processFolder(new DirectoryInfo(folder).GetFiles());
+
+            if (HadProblems)
+                ConsoleUtil.WriteLine("Warning: ".Color(ConsoleColor.Red) + "some errors have occurred during this run. Please review the stderr output for details.");
+
+            return HadProblems ? 1 : 0;
+        }
+
+        private static IEnumerable<FileInfo> findAllFilesRecursive()
+        {
+            var result = new List<FileInfo>();
+            foreach (var folder in Args.Folders)
+                addFolder(new DirectoryInfo(folder), result);
+            return result;
+        }
+
+        private static void addFolder(DirectoryInfo dir, List<FileInfo> files)
+        {
+            try
+            {
+                files.AddRange(dir.GetFiles());
+                foreach (var d in dir.GetDirectories())
+                    addFolder(d, files);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                ConsoleUtil.WriteLine("Error: ".Color(ConsoleColor.Red) + "not authorized to list directory contents: " + dir.FullName, stdErr: true);
+                HadProblems = true;
+            }
+            catch (Exception e)
+            {
+                ConsoleUtil.WriteLine("Error: ".Color(ConsoleColor.Red) + dir.FullName + ": " + e.Message, stdErr: true);
+                HadProblems = true;
+            }
+        }
+
+        private class info
+        {
+            public FileInfo File;
+            public DateTime Timestamp;
+            public string BareName;
+            public ConsoleColoredString ColoredName;
+            public bool Old = false;
+            public double Age { get { return (Now - Timestamp).TotalDays; } }
+
+            public info(FileInfo file, Match match)
+            {
+                File = file;
+                var groups = new[] { match.Groups["y"], match.Groups["m"], match.Groups["d"], match.Groups["th"], match.Groups["tm"], match.Groups["ts"] }.AsEnumerable();
+                groups = groups.Where(g => g.Success).OrderByDescending(g => g.Index);
+                ColoredName = File.Name.Color(ConsoleColor.DarkGray);
+                foreach (var group in groups)
+                    ColoredName = ColoredName.ColorSubstring(group.Index, group.Length, ConsoleColor.White);
+                if (match.Groups["g"].Success)
+                {
+                    var bareName = new StringBuilder();
+                    foreach (Capture capture in match.Groups["g"].Captures)
+                    {
+                        bareName.Append(capture.Value);
+                        ColoredName = ColoredName.ColorSubstring(capture.Index, capture.Length, ConsoleColor.Yellow);
+                    }
+                    BareName = bareName.ToString();
+                }
+                else
+                {
+                    var bareName = new StringBuilder(File.Name);
+                    foreach (var group in groups)
+                        bareName.Remove(group.Index, group.Length);
+                    BareName = bareName.ToString();
+                }
+                ColoredName = (File.FullName.Substring(0, File.FullName.Length - ColoredName.Length)).Color(ConsoleColor.DarkGray) + ColoredName;
+            }
+        }
+
+        private static void processFolder(IEnumerable<FileInfo> files)
+        {
+            if (Args.FilterRegex != null)
+                files = files.Where(f => Args.FilterRegex.IsMatch(f.FullName));
+            var groups = files
+                .Select(file => new { file, match = Args.TimestampFormatRegex.Match(file.Name) })
+                .Where(f => f.match.Success)
+                .Select(f => new info(f.file, f.match) { Timestamp = getTimestamp(f.file, f.match) })
+                .Where(f => f.Timestamp != default(DateTime))
+                .GroupBy(f => f.BareName);
+            foreach (var group in groups)
+            {
+                if (Args.Verbose)
+                    ConsoleUtil.WriteLine("Group: ".Color(ConsoleColor.Cyan) + group.Key);
+                // Apply max age
+                foreach (var file in group.Where(f => f.Age > Args.MaxAge))
+                    file.Old = true;
+                // Apply spacing
+                var remaining = group.Where(f => !f.Old).OrderByDescending(f => f.Age).ToList();
+                var prev = remaining.Count > 0 ? remaining[0] : null;
+                for (int i = 1; i < remaining.Count - 1; i++)
+                {
+                    var gap = prev.Age - remaining[i + 1].Age;
+                    var wantedGap = Args.SpacingFunc(remaining[i + 1].Age);
+                    if (gap <= wantedGap)
+                        remaining[i].Old = true;
+                    else
+                        prev = remaining[i];
+                }
+                // Now actually process the old files as required
+                foreach (var file in group.OrderByDescending(f => f.Age))
+                {
+                    if (Args.Verbose || file.Old)
+                    {
+                        var dbg = "";
+#if DEBUG
+                        try { dbg = ", gap {0:0.0}, wanted {1:0.0}".Fmt(file.Age - group.Where(f => !f.Old && f.Age < file.Age).Max(f => f.Age), Args.SpacingFunc(file.Age)); }
+                        catch { }
+#endif
+                        ConsoleUtil.Write("  " + file.ColoredName + ", ");
+                        ConsoleUtil.Write(file.Age.ToString("0.0") + " days old, ");
+                        ConsoleUtil.Write(file.Old ? "remove".Color(ConsoleColor.Red) : "keep".Color(ConsoleColor.Green));
+                        Console.WriteLine(dbg);
+                    }
+                    if (!file.Old)
+                        continue;
+                    bool executeOK = true;
+                    if (Args.Execute != null)
+                    {
+                        var runner = new CommandRunner();
+                        runner.Command = Args.Execute.Replace("{}", file.File.FullName);
+                        if (Args.Verbose)
+                        {
+                            runner.StdoutText += str => Console.Write(str);
+                            runner.StderrText += str => Console.Error.Write(str);
+                        }
+                        runner.Start();
+                        runner.EndedWaitHandle.WaitOne();
+                        executeOK = runner.ExitCode == 0;
+                        if (Args.Verbose)
+                            Console.WriteLine();
+                    }
+                    if (Args.Delete && executeOK)
+                        try { file.File.Delete(); }
+                        catch (FileNotFoundException) { }
+                        catch
+                        {
+                            ConsoleUtil.WriteLine("Error: ".Color(ConsoleColor.Red) + "could not delete file: " + file.File.FullName, stdErr: true);
+                            HadProblems = true;
+                        }
+                }
+            }
+        }
+
+        private static DateTime getTimestamp(FileInfo file, Match match)
+        {
+            if (!match.Groups["y"].Success && !match.Groups["m"].Success && !match.Groups["d"].Success)
+                return default(DateTime); // no warning in this case; the file simply doesn't have a timestamp
+            if (!match.Groups["y"].Success || !match.Groups["m"].Success || !match.Groups["d"].Success)
+            {
+                ConsoleUtil.WriteLine("Error: ".Color(ConsoleColor.Red) + "the {field}TimestampFormat{} matches a year, a month or a day, but not all of them. File {0}".Fmt(
+                    file.FullName), stdErr: true);
+                HadProblems = true;
+                return default(DateTime);
+            }
+            bool hasH = match.Groups["th"].Success;
+            bool hasM = match.Groups["tm"].Success;
+            bool hasS = match.Groups["ts"].Success;
+            if ((hasS && !hasM) || (hasM && !hasH))
+            {
+                ConsoleUtil.WriteLine("Error: ".Color(ConsoleColor.Red) + "the {field}TimestampFormat{} matches seconds but not minutes, or minutes but not hours. File {0}".Fmt(
+                    file.FullName), stdErr: true);
+                HadProblems = true;
+                return default(DateTime);
+            }
+            try
+            {
+                var y = int.Parse(match.Groups["y"].Value);
+                var m = int.Parse(match.Groups["m"].Value);
+                var d = int.Parse(match.Groups["d"].Value);
+                if (y < 100)
+                    y = (y < 80 ? 2000 : 1900) + y;
+                var th = hasH ? int.Parse(match.Groups["th"].Value) : 0;
+                var tm = hasM ? int.Parse(match.Groups["tm"].Value) : 0;
+                var ts = hasS ? int.Parse(match.Groups["ts"].Value) : 0;
+                return new DateTime(y, m, d, th, tm, ts);
+            }
+            catch
+            {
+                var time = (hasH ? match.Groups["th"].Value : "") + (hasM ? ":" + match.Groups["tm"].Value : "") + (hasS ? ":" + match.Groups["ts"].Value : "");
+                ConsoleUtil.WriteLine("Error: ".Color(ConsoleColor.Red) + "could not parse the timestamp ({0}) for file {1}".Fmt(
+                    "y {0}, m {1}, d {2}".Fmt(match.Groups["y"].Value, match.Groups["m"].Value, match.Groups["d"].Value) + (time == "" ? "" : (", time " + time)),
+                    file.FullName), stdErr: true);
+                HadProblems = true;
+                return default(DateTime);
+            }
+        }
+
+#if DEBUG
+        private static void PostBuildCheck(IPostBuildReporter rep)
+        {
+            CommandLineParser.PostBuildStep<CommandLine>(rep, null);
+        }
+#endif
+    }
+}
